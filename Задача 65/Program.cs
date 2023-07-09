657c110bf896c4b79e43400418e7817bb82ffb38
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2023-07-10 00:57:11 +0300

    выполнили задачу 65

--- Задача 65/Program.cs
+++ Задача 65/Program.cs
@@ -2,3 +2,24 @@
 ** Задача 65. Задайте числа M, N.
 ** Выведите все числа от M..N, используя рекурсию
 */
+
+int GetUserInput(string text) {
+    System.Console.Write(text);
+    int input = int.Parse(System.Console.ReadLine()!);
+    return input;
+}
+
+void PrintNumbers(int start, int end) {
+    if (start <= end) {
+        System.Console.Write(start);
+        start++;
+        PrintNumbers(start, end);
+    }
+}
+ void Main() {
+    int start = GetUserInput("Введите первое число: ");
+    int end = GetUserInput("Введите второе число: ");
+    PrintNumbers(start, end);
+ }
+
+ Main();
