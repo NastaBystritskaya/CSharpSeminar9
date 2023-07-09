e40319837d840b916e73da38d217c6655d77f466
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2023-07-09 23:17:03 +0300

    выполнили задачу 63

--- Задача 63/Program.cs
+++ Задача 63/Program.cs
@@ -2,3 +2,23 @@
 ** Задача 63. Задайте число N.
 ** Выведите все числа от 1..N, используя рекурсию
 */
+
+int GetUserInput() {
+    System.Console.Write("Введите число: ");
+    int input = int.Parse(System.Console.ReadLine()!);
+    return input;
+}
+
+void PrintNumbers(int start, int end) {
+    if (start <= end) {
+        System.Console.Write(start);
+        start++;
+        PrintNumbers(start, end);
+    }
+}
+ void Main() {
+    int input = GetUserInput();
+    PrintNumbers(1, input);
+ }
+
+ Main();
